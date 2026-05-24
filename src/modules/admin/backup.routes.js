1ba79862f138bc4801aa9402f0ee2a5169f479d0
Dripzoid <dripzoidapp@gmail.com>
2026-05-25 01:02:10 +0530

    Update backup.routes.js

--- src/modules/admin/backup.routes.js
+++ src/modules/admin/backup.routes.js
@@ -30,7 +30,7 @@ const upload = multer({
 
 router.get(
   "/export-db",
-  requireAdminAuth,
+  adminAuth,
   exportDatabase
 );
 
@@ -40,7 +40,7 @@ router.get(
 
 router.post(
   "/import-db",
-  requireAdminAuth,
+  adminAuth,
   upload.single("sqlfile"),
   importDatabase
 );
