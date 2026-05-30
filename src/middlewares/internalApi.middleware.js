c4e800ab20ab001fbbc7dad5d55c236377007bb5
Dripzoid <dripzoidapp@gmail.com>
2026-05-31 02:16:10 +0530

    Update internalApi.middleware.js

--- src/middlewares/internalApi.middleware.js
+++ src/middlewares/internalApi.middleware.js
@@ -2,20 +2,38 @@ export const verifyInternalApi =
   (req, res, next) => {
 
     const apiKey =
-      req.headers["x-internal-key"];
+      req.headers[
+        "x-internal-key"
+      ];
 
     if (
+      !apiKey ||
       apiKey !==
-      process.env.ASKDRIP_INTERNAL_KEY
+        process.env
+          .ASKDRIP_INTERNAL_KEY
     ) {
       return res.status(401).json({
         success: false,
-        message: "Unauthorized",
+        message:
+          "Unauthorized Internal Request",
+      });
+    }
+
+    const userId =
+      req.headers[
+        "x-user-id"
+      ];
+
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "X-User-Id header is required",
       });
     }
 
     req.internalUserId =
-      req.headers["x-user-id"];
+      userId;
 
     next();
   };
