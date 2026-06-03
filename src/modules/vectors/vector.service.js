dfa83523bae254d6a34a4279666785fc33d26d0d
Dripzoid <dripzoidapp@gmail.com>
2026-06-03 16:56:33 +0530

    Update vector.service.js

--- src/modules/vectors/vector.service.js
+++ src/modules/vectors/vector.service.js
@@ -1,56 +1,65 @@
 import prisma from "../../lib/prisma.js";
 
 export async function getAllProductVectors() {
+
   const products =
     await prisma.$queryRaw`
       SELECT
-       SELECT
-  pv.product_id,
-  pv.embedding::text AS embedding,
+        pv.product_id,
+        pv.embedding::text AS embedding,
 
-  p.id,
-  p.name,
-  p.slug,
-  p.description,
-  p."categoryId",
-  p.subcategory,
-  p."actualPrice",
-  p."originalPrice",
-  p.images,
-  p.colors,
-  p.rating,
-  p.stock,
-  p.sold,
-  p.featured,
-  p."createdAt",
-  p."updatedAt"
+        p.id,
+        p.name,
+        p.slug,
+        p.description,
+        p."categoryId",
+        p.subcategory,
+        p."actualPrice",
+        p."originalPrice",
+        p.images,
+        p.colors,
+        p.rating,
+        p.stock,
+        p.sold,
+        p.featured,
+        p."createdAt",
+        p."updatedAt"
 
-FROM product_vectors pv
-INNER JOIN "Product" p
-  ON p.id = pv.product_id::text
+      FROM product_vectors pv
+      INNER JOIN "Product" p
+        ON p.id = pv.product_id::text
     `;
 
-  return products.map((product) => ({
-    product_id: product.product_id,
+  return products.map(
+    (product) => ({
+      product_id:
+        product.product_id,
 
-    embedding: JSON.parse(
-      product.embedding
-    ),
+      embedding:
+        JSON.parse(
+          product.embedding
+        ),
 
-    product: {
-      id: product.product_id,
-      name: product.name,
-      slug: product.slug,
-      description: product.description,
-      category_id: product.category_id,
-      subcategory_id: product.subcategory_id,
-      actual_price: product.actual_price,
-      discount_price: product.discount_price,
-      images: product.images,
-      gender: product.gender,
-      color: product.color
-    }
-  }));
+      product: {
+        id: product.id,
+        name: product.name,
+        slug: product.slug,
+        description: product.description,
+        categoryId: product.categoryId,
+        subcategory: product.subcategory,
+        actualPrice: product.actualPrice,
+        originalPrice: product.originalPrice,
+        images: product.images,
+        colors: product.colors,
+        rating: product.rating,
+        stock: product.stock,
+        sold: product.sold,
+        featured: product.featured,
+        createdAt: product.createdAt,
+        updatedAt: product.updatedAt
+      }
+    })
+  );
 }
 
 export async function getAllKBVectors() {
