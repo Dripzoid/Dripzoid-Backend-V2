bff70007385fd8b6a91be7dbd5256c42b7a773bf
Dripzoid <dripzoidapp@gmail.com>
2026-06-03 19:34:04 +0530

    Update semantic.routes.js

--- src/modules/semantic-search/semantic.routes.js
+++ src/modules/semantic-search/semantic.routes.js
@@ -1,15 +1,15 @@
 import express from "express";
 
 import {
-  searchProducts,
-} from "./semantic.controller.js";
+  embedText,
+} from "./embedding.controller.js";
 
 const router =
   express.Router();
 
 router.post(
-  "/products/search",
-  searchProducts
+  "/",
+  embedText
 );
 
 export default router;
